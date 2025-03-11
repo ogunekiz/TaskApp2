fdcae052f3f951461f63daa2b873aa34a8609da3
Ogün EKİZ <ogunekiz@hotmail.com>
2025-03-11 22:21:49 +0300

    Add project files.

---
+++ TaskApp/Controllers/TasksController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskApp.Models;
+
+namespace TaskApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private static readonly List<TaskItem> tasks = new()
+    {
+        new TaskItem { Id = 1, Title = "Alışveriş yap", IsCompleted = false },
+        new TaskItem { Id = 2, Title = "Rapor hazırla", IsCompleted = true },
+        new TaskItem { Id = 3, Title = "Spor yap", IsCompleted = false },
+        new TaskItem { Id = 4, Title = "Kitap oku", IsCompleted = true }
+    };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TaskItem>> GetTasks()
+        {
+            return Ok(tasks);
+        }
+    }
+}
---
+++ TaskApp/Program.cs
@@ -0,0 +1,50 @@
+namespace TaskApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/")
+                {
+                    context.Response.Redirect("/swagger/index.html");
+                    return;
+                }
+                await next();
+            });
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
