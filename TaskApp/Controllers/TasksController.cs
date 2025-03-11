1b747e07f0b43b9f5596bacd422c40189e2547f5
Ogün EKİZ <ogunekiz@hotmail.com>
2025-03-12 00:11:08 +0300

    add new data

--- TaskApp/Controllers/TasksController.cs
+++ TaskApp/Controllers/TasksController.cs
@@ -15,7 +15,8 @@ namespace TaskApp.Controllers
         new TaskItem { Id = 4, Title = "Kitap oku", IsCompleted = true },
         new TaskItem { Id = 5, Title = "Koşu yap", IsCompleted = true },
         new TaskItem { Id = 6, Title = "Dans et", IsCompleted = false },
-        new TaskItem { Id = 7, Title = "Araştırma yap", IsCompleted = true }
+        new TaskItem { Id = 7, Title = "Araştırma yap", IsCompleted = true },
+        new TaskItem { Id = 8, Title = "Makale yaz", IsCompleted = false }
     };
 
         [HttpGet]
